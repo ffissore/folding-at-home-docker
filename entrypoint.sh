38ae58b371d335f3964a0727227e513b365c5f97
Federico Fissore <federico@fissore.org>
2020-03-16 13:17:07 +0100

    Ported to balena

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,15 @@
 set -e
 
 /opt/fahclient/FAHClient \
-    --user="${USER}" \
-    --team="${TEAM}" \
-    --passkey="${PASSKEY}" \
-    --gpu="${ENABLE_GPU}" \
-    --smp="${ENABLE_SMP}" \
+    --user="${FAH_USER}" \
+    --team="${FAH_TEAM}" \
+    --passkey="${FAH_PASSKEY}" \
+    --gpu="${FAH_ENABLE_GPU}" \
+    --smp="${FAH_ENABLE_SMP}" \
     --power=full \
     --gui-enabled=false \
+    --data-directory=/fah-data \
+    --cpu-affinity=true \
+    --allow=0/0 \
+    --web-allow=0/0 \
     "${@}"
